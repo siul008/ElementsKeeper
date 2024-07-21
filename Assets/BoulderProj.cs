9fdf79425d5ffdb17c780429fa536a43a61369ab
siul008 <Julien.nunespro@gmail.com>
2024-07-21 09:37:08 +0200

    Boulder Tower

---
+++ Assets/BoulderProj.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderProj : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 5f;
+    [SerializeField] float damage;
+    [SerializeField] float knockbackForce;
+    List<GameObject> enemiesHit = new List<GameObject>();
+
+    void Start()
+    {
+        Destroy(gameObject, 8f);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * bulletSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy" && !enemiesHit.Contains(other.gameObject))
+        {
+            enemiesHit.Add(other.gameObject);
+            other.GetComponent<Enemy>().TakeDamage(damage);
+            other.GetComponent<Rigidbody2D>().AddForce(Vector2.right * knockbackForce, ForceMode2D.Impulse);
+        }
+    }
+}
+
+
