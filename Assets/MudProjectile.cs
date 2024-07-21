568491e58028f511d2c19b436e65591e15ac3a77
siul008 <Julien.nunespro@gmail.com>
2024-07-21 08:52:48 +0200

    MudTower

---
+++ Assets/MudProjectile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudProjectile : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 5f;
+    [SerializeField] float damage;
+    [SerializeField] GameObject mudPound;
+
+    bool hitEnemy = false;
+
+    void Start()
+    {
+        Destroy(gameObject, 8f);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * bulletSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (!hitEnemy)
+            {
+                hitEnemy = true;
+                other.GetComponent<Enemy>().TakeDamage(damage);
+                Instantiate(mudPound, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
