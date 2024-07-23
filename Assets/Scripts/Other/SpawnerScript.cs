304e495a74982467c04f71422fb68ad79c2770ab
siul008 <86720411+siul008@users.noreply.github.com>
2024-07-23 10:41:05 +0200

    WAves

--- Assets/Scripts/Other/SpawnerScript.cs
+++ Assets/Scripts/Other/SpawnerScript.cs
@@ -6,31 +6,59 @@ public class SpawnerScript : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
     [SerializeField] Transform[] spawns;
-    [SerializeField] float spawnRate = 1f;
-    float cooldown = 1f;
-    // Start is called before the first frame update
+    float spawnTime;
+    [SerializeField]
+    float endOfWaveDelay;
+    int index;
+    public int remaining;
+
+    [SerializeField]
+    List<Wave> waves = new List<Wave>();
+
     void Start()
     {
-        
+        index = 0;
+        remaining = waves[index].enemiesNbr;
+        spawnTime = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        cooldown -= Time.deltaTime;
-        if(cooldown <= 0)
+        if (spawnTime >= waves[index].spawnRate && remaining > 0)
         {
-            cooldown = spawnRate;
-            spawnRate *= 0.99f;
             Spawn();
         }
+        else
+        {
+            spawnTime += Time.deltaTime;
+        }
+    }
+
+    IEnumerator NextWave()
+    {
+        yield return new WaitForSeconds(endOfWaveDelay);
+        index++;
+        remaining = waves[index].enemiesNbr;
+        spawnTime = 0;
     }
 
     void Spawn()
     {
+        spawnTime = 0;
         Vector3 spawnPoint = spawns[Random.Range(0, spawns.Length)].position;
         spawnPoint.y = Random.Range(spawnPoint.y - 0.10f, spawnPoint.y + 0.11f);
         Instantiate(enemy, spawnPoint, Quaternion.identity);
+        remaining--;
+        if (remaining <= 0)
+        {
+            StartCoroutine(NextWave());
+        }
     }
 
+    [System.Serializable]
+    public class Wave 
+    {
+        public int enemiesNbr;
+        public float spawnRate;
+    }
 }
