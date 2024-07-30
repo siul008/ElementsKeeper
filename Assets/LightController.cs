7784ccbcc878b4ee009401f600c96ef77caf02a1
siul008 <86720411+siul008@users.noreply.github.com>
2024-07-30 17:42:21 +0200

    Scene and light

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -1,61 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class LightController : MonoBehaviour
 {
-    bool decreasing;
-    bool increasing;
-    public Vector3 lightDown = new Vector3(0.1f, 0.1f, 0.1f);
-    public Vector3 lightUp = new Vector3(0.3f, 0.3f, 0.3f);
-    public float duration = 100f; 
+    [SerializeField] Light2D lightHolder;
+    [SerializeField] float intensityOn;
+    [SerializeField] float fallOffOn;
+    [SerializeField] Vector3 scaleOn = new Vector3(0.3f, 0.3f, 0.3f);
+    [SerializeField] float intensityOff;
+    [SerializeField] float fallOffOff;
+    [SerializeField] Vector3 scaleOff = new Vector3(0.1f, 0.1f, 0.1f);
 
-    private Vector3 initialScale;
-    private Vector3 targetScale;
-    private float elapsedTime;
+    public float turnOffDuration = 100f;
+    public float turnOnDuration = 100f;
 
-    private void Start()
+    public IEnumerator TurnOffLight()
     {
-        increasing = false;
-        decreasing = true;
-        initialScale = transform.localScale;
-        targetScale = Vector3.zero;
-        elapsedTime = 0f;
+        yield return new WaitForSeconds(turnOffDuration);
+        transform.localScale = scaleOff;
+        lightHolder.intensity = intensityOff;
+        lightHolder.falloffIntensity = fallOffOff;
     }
 
-    void Update()
+    public IEnumerator TurnOnLight()
     {
-        if (increasing)
-        {
-            transform.localScale += lightUp * Time.deltaTime;
-            if (transform.localScale.x > 1)
-            {
-                transform.localScale = Vector3.one;
-                increasing = false;
-                decreasing = true;
-                elapsedTime = 0f; 
-            }
-        }
-        else if (decreasing)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = elapsedTime / duration;
-            transform.localScale = Vector3.Lerp(initialScale, targetScale, t);
-
-            if (transform.localScale.x <= 0)
-            {
-                transform.localScale = Vector3.zero;
-                decreasing = false;
-                increasing = false;
-            }
-        }
+        yield return new WaitForSeconds(turnOnDuration);
+        transform.localScale = scaleOn;
+        lightHolder.intensity = intensityOn;
+        lightHolder.falloffIntensity = fallOffOn;
     }
 
-    public void ResetLight()
+    public float GetTurnOffDuration()
     {
-        increasing = true;
-        decreasing = false;
-        transform.localScale = initialScale; 
-        elapsedTime = 0f;
+        return (turnOffDuration);
     }
 }
--- Assets/SpawnerScriptV2.cs
+++ Assets/SpawnerScriptV2.cs
@@ -14,40 +14,185 @@ public class SpawnerScriptV2 : MonoBehaviour
 
     }
 
-    [SerializeField]
-    DifficultyTier[] tiers;
+    enum WaveState
+    {
+        PROGRESS,
+        SPAWNING,
+        WAITINGBIGWAVE,
+        BIGWAVE
+    }
+
+    [SerializeField] LightController playerLight;
+    [SerializeField] DifficultyTier[] tiers;
+    [SerializeField] Transform[] lanesTransform;
     DifficultyTier wave;
     float spawnTime;
     int difficultyIndex;
-
+    WaveState state;
+    bool lightCalled;
+    [SerializeField] float tierDuration;
+    [SerializeField] float minMultSpawnRate;
+    float lightDuration;
+    float tierTime;
+       
     void Start()
     {
+        tierTime = 0;
         difficultyIndex = -1;
         ProgressNextTier();
+        state = WaveState.PROGRESS;
+        lightCalled = false;
+        lightDuration = playerLight.GetTurnOffDuration();
     }
 
     private void Update()
     {
-        if (spawnTime > wave.spawnRate)
+        if (spawnTime >= wave.spawnRate * GetCurrentSpawnRateMult() && state == WaveState.PROGRESS)
         {
-
+            state = WaveState.SPAWNING;
+            ChooseSpawn();
         }
         else
         {
+            spawnTime += Time.deltaTime;
+        }
+        if (tierTime >= tierDuration - lightDuration)
+        {
+            playerLight.TurnOffLight();
+            lightCalled = true;
+        }
+        if (tierTime >= tierDuration && state == WaveState.PROGRESS)
+        {
+            state = WaveState.WAITINGBIGWAVE;
+        }
+        else
+        {
+            tierTime += Time.deltaTime;
+        }
+        if (state == WaveState.WAITINGBIGWAVE && NoEnemyLeft())
+        {
+            StartBigWave();
+        }
+        if (state == WaveState.BIGWAVE && NoEnemyLeft())
+        {
+            playerLight.TurnOnLight();
+            ProgressNextTier();
+        }
+
+    }
+
+    bool NoEnemyLeft()
+    {
+        return (GameObject.FindGameObjectsWithTag("Enemy").Length == 0);
+    }
+
+    private void ChooseSpawn()
+    {
+        //Is a horde spawn
+        if (Random.Range(0, 101) <= wave.hordeChance)
+        {
+            DifficultyTier tier;
+            //Is a horde from the tier bellow
+            if (difficultyIndex > 0 && Random.Range(0, 101) <= wave.bellowTierHordeChance)
+            {
+                tier = tiers[difficultyIndex - 1];
+            }
+            //Is a horde from the current wave
+            else
+            {
+                tier = wave;
+            }
+            HordePrefab randomHorde = tier.horde[Random.Range(0, tier.horde.Count)];
+            Transform randomLane = GetRandomLane(tier);
+
+            Spawn(randomLane, randomHorde.hordePrefab);
+            spawnTime = 0 - randomHorde.breathingTime;
+        }
+        //Is a basic enemy spawn
+        else
+        {
+            Transform randomLane = GetRandomLane(wave);
+            GameObject randomEnemy = GetRandomEnemy(wave);
+
+            Spawn(randomLane, randomEnemy);
             spawnTime = 0;
         }
     }
 
-    private void Spawn()
+    float GetCurrentSpawnRateMult()
     {
+        return (1 - (tierTime * (1 - minMultSpawnRate) / tierDuration));
+    }
 
+    void Spawn(Transform lanePos, GameObject enemy)
+    {
+        Instantiate(enemy, lanePos.position, Quaternion.identity);
+        state = WaveState.PROGRESS;
+    }
+
+    GameObject GetRandomEnemy(DifficultyTier tier)
+    {
+        int totalWeight = 0;
+
+        foreach (EnemyEntity enemy in tier.enemies)
+        {
+            totalWeight += enemy.weight;
+        }
+        float randomValue = Random.Range(0, totalWeight);
+        float cumulativeWeight = 0;
+        foreach (EnemyEntity enemy in tier.enemies)
+        {
+            cumulativeWeight += enemy.weight;
+            if (randomValue <= cumulativeWeight)
+            {
+                return enemy.enemyPrefab;
+            }
+        }
+        Debug.LogError("No enemy returned");
+        return null;
+    }
+
+    Transform GetRandomLane(DifficultyTier tier)
+    {
+        int totalWeight = 0;
+
+        foreach (LanesSelector lane in tier.lanes)
+        {
+            totalWeight += lane.weight;
+        }
+        float randomValue = Random.Range(0, totalWeight);
+        float cumulativeWeight = 0;
+        foreach (LanesSelector lane in tier.lanes)
+        {
+            cumulativeWeight += lane.weight;
+            if (randomValue <= cumulativeWeight)
+            {
+                return lanesTransform[(int)lane.lane];
+            }
+        }
+        Debug.LogError("No lanes returned");
+        return lanesTransform[0];
+    }
+
+    void StartBigWave()
+    {
+        Instantiate(wave.endWave, lanesTransform[0].position, Quaternion.identity);
+        state = WaveState.BIGWAVE;
     }
 
     void ProgressNextTier()
     {
-        spawnTime = 0;
-        difficultyIndex++;
+        tierTime = 0;
+        if (spawnTime > 0)
+        {
+            spawnTime = 0;
+        }
+        if (difficultyIndex + 1 < tiers.Length)
+        {
+            difficultyIndex++;
+        }
         wave = tiers[difficultyIndex];
+        lightCalled = true;
     }
 
     [System.Serializable]
@@ -55,7 +200,9 @@ public class SpawnerScriptV2 : MonoBehaviour
     {
         public string name;
         public float hordeChance;
+        public float bellowTierHordeChance;
         public float spawnRate;
+        public GameObject endWave;
         public List<LanesSelector> lanes;
         public List<HordePrefab> horde;
         public List<EnemyEntity> enemies;
@@ -73,14 +220,14 @@ public class SpawnerScriptV2 : MonoBehaviour
     class EnemyEntity
     {
         public GameObject enemyPrefab;
-        public float weight;
+        public int weight;
     }
 
     [System.Serializable]
     class LanesSelector
     {
         public Lanes lane;
-        public float weight;
+        public int weight;
 
     }
 }
